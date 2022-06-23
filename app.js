ce84e788016ad8527f3761c1d38e46bbb89430ce
Tung Tran Thanh <intern.tthanhtung1@tma.com.vn>
2022-06-23 11:00:27 +0700

    Minor changes for deployment

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 
 const indexRouter = require('./routes/index');
